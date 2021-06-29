08aecc176a008c33bdf67eab06032e9104831c21
wl <kcwl558106@gmail.com>
2021-06-29 14:01:34 +0800

    c12-18

--- Game.kt
+++ Game.kt
@@ -1,4 +1,4 @@
-/*c12-18*/
+/*c12-19*/
 import java.lang.Math.pow
 import java.lang.Math.random
 
@@ -30,30 +30,6 @@ private fun printPlayerStatus(
 
     println(statusFormatString)
 }
-/*
-private fun auraColor(karma: Int)=
-    when (karma) {
-        in (0..5) -> "紅色"
-        in (6..10) -> "橘黃色"
-        in (11..15) -> "紫色"
-        in (16..20) -> "綠色"
-        else -> "無光環"
-    }
-
-private fun formaHealthStatus(healthPoints: Int, isBlessed: Boolean) =
-    when (healthPoints) {
-        100 -> "健康狀態極佳"
-        in 90..99 -> "有一些小擦傷"
-        in 75..89 -> if (isBlessed) {
-            "雖有一些傷口，但恢復很快"
-        } else {
-            "有一些傷口"
-        }
-        in 15..74 -> "嚴重受傷"
-        //顯示玩家狀態
-        else -> "情況不妙"
-    }
-*/
 
 private fun drunkenness(x: Int) {
     val s = when (x) {
--- Player.kt
+++ Player.kt
@@ -1,4 +1,4 @@
-/*c12-18*/
+/*c12-19*/
 class Player {
     var name = "wl"
         get() = field.capitalize()
@@ -14,4 +14,26 @@ class Player {
         println("FireBall杯數：$numFireballs")
         return numFireballs
     }
+    private fun auraColor(karma: Int)=
+        when (karma) {
+            in (0..5) -> "紅色"
+            in (6..10) -> "橘黃色"
+            in (11..15) -> "紫色"
+            in (16..20) -> "綠色"
+            else -> "無光環"
+        }
+
+    private fun formaHealthStatus(healthPoints: Int, isBlessed: Boolean) =
+        when (healthPoints) {
+            100 -> "健康狀態極佳"
+            in 90..99 -> "有一些小擦傷"
+            in 75..89 -> if (isBlessed) {
+                "雖有一些傷口，但恢復很快"
+            } else {
+                "有一些傷口"
+            }
+            in 15..74 -> "嚴重受傷"
+            //顯示玩家狀態
+            else -> "情況不妙"
+        }
 }
